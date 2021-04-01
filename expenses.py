c925fe75d66eb696f2db21fee3505134c56662e4
kbermet <kbermet@gmail.com>
2021-04-01 20:23:42 +0600

    initial commit

---
+++ expenses.py
@@ -0,0 +1,21 @@
+expense = 0
+
+
+def costs(day):
+    global expense
+    expense += day
+
+
+costs(17)
+costs(20)
+costs(21)
+print(expense)
+
+
+def costs2(*expense1):
+    for day in expense1:
+        day += day
+    return day
+
+
+print(costs2(56, 14, 17))
